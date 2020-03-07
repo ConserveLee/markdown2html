ffdec3f1d5e149616eba10b9dc0dee26d303e14f
leeli <leeli@xiaoe-tech.com>
2020-03-08 02:34:51 +0800

    修改

--- md2html/entrypoint.sh
+++ md2html/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-pandoc /go/src/markdownHtml/web/data/index.md \
-      --output=/go/src/markdownHtml/web/data/index.html \
-      --to=html5 \
-      --css=/go/src/markdownHtml/css/tufte.css \
-      --metadata pagetitle="自定义标题" \
-      --highlight-style=haddock \
-      --self-contained
+/go/src/markdownHtml/make.sh
 cd ./web/
 nohup /go/src/markdownHtml/web/server &
 cd ../
